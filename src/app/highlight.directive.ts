97f2ece47e147d93022e4fd8354cc01d605a2f5c
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-09-28 19:10:47 +0300

    highlight element

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,22 +1,13 @@
-import { Directive,ElementRef,HostListener } from '@angular/core';
+import { Directive,ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef){}
-
-  @HostListener("click") onClicks(){
-    this.textDeco("line-through")
+  constructor( elem:ElementRef){
+    elem.nativeElement.style.backgroundColor="#9DC183"
   }
 
-  @HostListener("dblclick") onDoubleClicks(){
-    this.textDeco("None")
   }
 
-
-  private textDeco(action:string){
-    this.elem.nativeElement.style.textDecoration=action;
-  }
-}
